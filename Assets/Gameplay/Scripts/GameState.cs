2981226235308b6b8b03a29faca490a6a4b8a025
lieroes <lars-betting@protonmail.com>
2023-02-04 19:35:27 +0000

    Added sounds, implemented background sound

--- Assets/Gameplay/Scripts/GameState.cs
+++ Assets/Gameplay/Scripts/GameState.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//wwise 
+using AK;
 public class GameState : MonoBehaviour
 {
     private Sun sun;
 
     private Cloud[] clouds;
 
+    //wwise
+    public AK.Wwise.Event playBackgroundEvent;
+    
     bool enableSun = true;
     // Start is called before the first frame update
     void Start()
     {
         sun = FindObjectOfType<Sun>();
         clouds = FindObjectsOfType<Cloud>();
+        
+        // play background sound. Included this in this script cause this script is called at the start of the game
+        playBackgroundEvent.Post(gameObject);
     }
 
     // Update is called once per frame
