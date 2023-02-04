c429706503d178b507ba199c23936736263376a5
DeclanMcG <mcguinness.declan0@gmail.com>
2023-02-04 16:13:23 +0000

    Added mouse cursor function (no click yet)
    and removed auto-generated files from being tracked

--- Assets/Gameplay/Scripts/CameraController.cs
+++ Assets/Gameplay/Scripts/CameraController.cs
@@ -12,6 +12,9 @@ public class CameraController : MonoBehaviour
 
     private float cameraRotY = 0f;
 
+    private Vector3 mouseScreenPos;
+    private Vector3 mouseWorldPos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +23,21 @@ public class CameraController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        //Camera movement
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
 
         cameraRotY -= mouseX;
         cameraRotY = Mathf.Clamp(cameraRotY, -CameraRotateLimit, CameraRotateLimit);
 
         transform.localRotation = Quaternion.Euler(gameObject.transform.localEulerAngles.x, -cameraRotY, gameObject.transform.localEulerAngles.z);
+
+        //Mouse movement
+        mouseScreenPos = Input.mousePosition;
+        Ray ray = Camera.main.ScreenPointToRay(mouseScreenPos);
+        if (Physics.Raycast(ray, out RaycastHit hitData)) 
+        {
+            //mouseWorldPos now contains the hit location
+            mouseWorldPos = hitData.point;
+        }
     }
 }
